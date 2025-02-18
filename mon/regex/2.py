be44f8541e27e4c13337830fe852d537a91febea
Nesvetailov Artem <75523744+Dibrit@users.noreply.github.com>
2025-02-18 13:43:04 +0500

    Delete mon/regex directory

--- mon/regex/2.py
+++
@@ -1,8 +0,0 @@
-import re
-
-a = input()
-c = r'^ab{2, 3}$'
-if re.fullmatch(c, a):
-    print("yes")
-else:
-    print("no")
--- mon/regex/3.py
+++
@@ -1,8 +0,0 @@
-import re
-
-a = input()
-c = r'^[a-z]+_[a-z]+$'
-if re.fullmatch(c, a):
-    print("yes")
-else:
-    print("no")
